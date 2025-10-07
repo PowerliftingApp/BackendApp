3593f7970c405413f812e543d2ff4d9bba8ded74
Santiago Cortés <santiago.cortes@qualitacorp.com>
2025-10-07 08:15:36 -0500

    feat: mejorar configuración de CORS al permitir métodos y encabezados adicionales

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  // Enable CORS with specific options
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5173', "https://powermind.site" , "https://www.powermind.site"],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: [
+      'http://localhost:5173',
+      'https://powermind.site',
+      'https://www.powermind.site'
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
   };
 
-  const app = await NestFactory.create(AppModule, {cors: corsOptions});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.PORT ?? 3000);
 }
